07fcb77ba343f94d474595549db88fa4d9fdeed0
ombhilare999 <ombhilare999@gmail.com>
2021-08-09 17:17:59 +0530

    bw-spi script updated and spi-test added

--- load_fw/README.md
+++ load_fw/README.md
@@ -1,9 +1,25 @@
 # BeagleWire Programming 
 
+- First of all add following lines in `.bashrc` so that those scripts can be run from anywhere.
+- `vim .bashrc`
+
+```
+# To run beaglewire script from anywhere
+ export PATH=$PATH:/home/debian/beaglewire/load_fw
+```
 
 - There are several ways for programming bitstream into BeagleWire:
 
-## 1) LKM module with the ice40-spi kernel driver:
+
+## 1) Flashing the sram on beaglewire with bitstream:
+
+```
+    bw-spi.sh <bin file to program>
+    #Example: bw-spi.sh blink.bin
+``` 
+
+
+## 2) LKM module with the ice40-spi kernel driver:
 
 - For this you need to load following DTS file into `uEnv.txt`
 
@@ -21,35 +37,5 @@
 - `make`
 - `./bw-prog.sh blink.bin` 
 - Using this way one can program any bitstream directly to the FPGA via ice40-spi kernel driver and LKM module.
-
-## 2) Flashing the sram on beaglewire with bitstream:
-
-- For this first you need to load following DTS file into `uEnv.txt`
-
-```
-    sudo vim /boot/uEnv.txt
-    
-    #Add this overlay line at addr4 place:
-
-    uboot_overlay_addr4=/lib/firmware/BW-ICE40Cape-flash-00A0.dtbo 
-
-    #Reboot after this
-``` 
-- `cd beaglewire/load_fw`
-- `./bw-spi.sh blink.bin`
-- Now we need to tristate the spi line so it doesn't interfere with spi bootup. For that we have one overlay:
-
-```
-    sudo vim /boot/uEnv.txt
-    
-    #Add this overlay line at addr4 place:
-
-    uboot_overlay_addr4=/lib/firmware/BW-ICE40Cape-nospi-00A0.dtbo 
-
-    #Reboot after this
-```
-- Reset the FPGA now the sram is flashed with bitstream. 
-
-
 
 
--- load_fw/bw-spi.sh
+++ load_fw/bw-spi.sh
@@ -4,18 +4,34 @@
 ## Script to prgoram beaglewire with spi
 ##########################################
 
+echo "|--------------------------------------------|"
+echo "|-----Flashing Beaglewire with SPI MODE -----|"
+echo "|--------------------------------------------|"
+
 echo "Truncating file to max limit of 4194304"
 truncate $1 -s 4194304 
 
-echo "Changing Direction of BB Reset to Output"
-echo 117 > /sys/class/gpio/export
-echo out > /sys/class/gpio/gpio117/direction
+echo "Changing Direction of BB Reset to Output so FPGA won't interfer in SPI FLASH Programming"
+echo 117 > /sys/class/gpio/export || echo "GPIO export of resest pin failed, please rerun"
+echo out > /sys/class/gpio/gpio117/direction || echo "GPIO out of reset pin failed, please rerun"
+
+echo "Activating SPI mode"
+config-pin P9_28 spi_cs
+config-pin P9_31 spi_sclk
 
 echo "Activating the flash chip with 0xAB command"
-./spi_test -d /dev/spidev1.0 -l 1 -m ab
+spi_test -d /dev/spidev1.0 -l 1 -m ab || echo "Failed, Rerun the script"
 
 echo "Flashing nor flash with the $1 bin file"
-flashrom -p linux_spi:dev=/dev/spidev1.0,spispeed=25000000 -w $1 -c MX25L3273E
+flashrom -p linux_spi:dev=/dev/spidev1.0,spispeed=25000000 -w $1 -c MX25L3273E || echo "Failed, Rerun the script"
 
 echo "Changing Direction of BB Reset to input"
 echo in > /sys/class/gpio/gpio117/direction
+
+echo "Deactivating SPI mode"
+config-pin P9_28 gpio
+config-pin P9_31 gpio
+
+echo "|---------------------------------------------|"
+echo "|----RESET THE FPGA to run the bitstream -----|"
+echo "|---------------------------------------------|"
